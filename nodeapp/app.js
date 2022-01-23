116cff6f912a3685843ac1ba5cdfdfc13646411b
0Ahmedsherif <89992111+0Ahmedsherif@users.noreply.github.com>
2022-01-23 19:31:13 +0200

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello World from my first iti')
+    res.send('Hello World from my first ITI!')
   })
   
   app.listen(port, () => {
